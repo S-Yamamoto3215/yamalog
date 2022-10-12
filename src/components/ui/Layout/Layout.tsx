7a4118fda9e9b7e1fa156ebfe762abd87f670c36
S-Yamamoto3215 <yamamotoshoya8@gmail.com>
2022-10-13 00:31:51 +0900

    コンポーネント分割/ディレクトリ整理

---
+++ src/components/ui/Layout/Layout.tsx
@@ -0,0 +1,13 @@
+import { FC, ReactNode } from 'react'
+import { Footer } from '@/components/ui/Footer'
+import { Header } from '@/components/ui/Header'
+
+export const Layout: FC<{ children: ReactNode }> = ({ children }) => {
+  return (
+    <>
+      <Header />
+      <main>{children}</main>
+      <Footer />
+    </>
+  )
+}
