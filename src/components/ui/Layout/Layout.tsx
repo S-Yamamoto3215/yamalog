17e560f78d6526da17aca6b0e93735a4bfb0a72c
S-Yamamoto3215 <yamamotoshoya8@gmail.com>
2022-10-23 15:50:04 +0900

    ベースのスタイル定義

--- src/components/ui/Layout/Layout.tsx
+++ src/components/ui/Layout/Layout.tsx
@@ -3,11 +3,13 @@ import { FC, ReactNode } from 'react'
 import { Footer } from '@/components/ui/Footer'
 import { Header } from '@/components/ui/Header'
 
+import styles from './layout.module.scss'
+
 export const Layout: FC<{ children: ReactNode }> = ({ children }) => {
   return (
     <>
       <Header />
-      <main>{children}</main>
+      <main className={styles.main}>{children}</main>
       <Footer />
     </>
   )
