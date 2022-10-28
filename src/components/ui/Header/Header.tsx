904f256cfc9b06a1a378a53607d22e6c086dae1f
S-Yamamoto3215 <yamamotoshoya8@gmail.com>
2022-10-28 22:49:01 +0900

    Header レイアウト

--- src/components/ui/Header/Header.tsx
+++ src/components/ui/Header/Header.tsx
@@ -1,9 +1,15 @@
 import { FC } from 'react'
+import { FaBars } from 'react-icons/fa'
+
+import styles from './header.module.scss'
 
 export const Header: FC = () => {
   return (
-    <header>
-      <h1>Header</h1>
+    <header className={styles.header}>
+      <h1 className={styles.logo}>yamalog</h1>
+      <div className={styles.iconWrap}>
+        <FaBars className={styles.menuIcon} />
+      </div>
     </header>
   )
 }
