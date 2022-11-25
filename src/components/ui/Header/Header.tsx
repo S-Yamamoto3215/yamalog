27e0dc56c7910d3fe113a7a1a0fadc954ad6bb14
S-Yamamoto3215 <yamamotoshoya8@gmail.com>
2022-11-25 15:05:50 +0900

    Header base styles

--- src/components/ui/Header/Header.tsx
+++ src/components/ui/Header/Header.tsx
@@ -1,9 +1,22 @@
 import { FC } from 'react'
 
+import styles from './header.module.scss'
+
 export const Header: FC = () => {
   return (
-    <header>
-      <h1>yamalog</h1>
+    <header className={styles.header}>
+      <div className={styles.header_logo}>
+        <h1>yamalog</h1>
+      </div>
+      <nav className={styles.header_nav}>
+        <ul className={styles.header_nav_menu}>
+          <li className={styles.header_nav_item}>About</li>
+          <li className={styles.header_nav_item}>Blog</li>
+          <li className={styles.header_nav_item}>Twitter</li>
+          <li className={styles.header_nav_item}>GitHub</li>
+          <li className={styles.header_nav_item}>Facebook</li>
+        </ul>
+      </nav>
     </header>
   )
 }
