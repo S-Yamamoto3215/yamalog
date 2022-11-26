ff75a1fe8b22286820700d148e40924df02ad1e4
S-Yamamoto3215 <yamamotoshoya8@gmail.com>
2022-11-26 21:10:16 +0900

    Home hero styling

--- src/components/page/Home/Home.tsx
+++ src/components/page/Home/Home.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import { FC } from 'react'
 
 import styles from './home.module.scss'
@@ -5,7 +6,23 @@ import styles from './home.module.scss'
 export const Home: FC = () => {
   return (
     <div className={styles.home}>
-      <h1>Home</h1>
+      <section className={styles.home_hero}>
+        <div className={styles.home_hero_container}>
+          <h1 className={styles.home_hero_title}>YamaLog</h1>
+          <div className={styles.home_hero_image}>
+            <Image
+              src='/images/home/hero.png'
+              width={300}
+              height={300}
+              alt='ノートパソコンのイラスト'
+            />
+          </div>
+          <p className={styles.home_hero_lead}>
+            Web Developer Shoya Yamamoto&apos;s Portfolio.
+          </p>
+        </div>
+      </section>
+      <div>なにかコンテンツが入る</div>
     </div>
   )
 }
